aad23ac415a22a6fa4e932692b5128dd58363131
alucardthefish <sergiop18@gmail.com>
2020-02-27 23:50:23 -0500

    Add logic for creating date time entry of deposition

--- app/src/main/java/com/sop/cacapp/Fragments/MainFragment.java
+++ app/src/main/java/com/sop/cacapp/Fragments/MainFragment.java
@@ -4,19 +4,39 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.sop.cacapp.Persistence.PoopOccurrencePersistent;
 import com.sop.cacapp.R;
 
 
 public class MainFragment extends Fragment {
+
+    private Button btnAddPoop;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.main_fragment, container, false);
+        final View view = inflater.inflate(R.layout.main_fragment, container, false);
+        btnAddPoop = view.findViewById(R.id.btnAddPoopRecord);
+        btnAddPoop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(view.getContext(), "Creando deposición", Toast.LENGTH_SHORT).show();
+                PoopOccurrencePersistent dbAccessor = new PoopOccurrencePersistent();
+                dbAccessor.CreatePoopOccurrence(view);
+                /*if (dbAccessor.CreatePoopOccurrence()) {
+                    Toast.makeText(view.getContext(), "Exitosamente agregado a poop diary", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(view.getContext(), "No se pudo agregar a poop diary", Toast.LENGTH_LONG).show();
+                }*/
+            }
+        });
         return view;
     }
 }
