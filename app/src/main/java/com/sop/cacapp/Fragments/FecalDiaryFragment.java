b6f4cf9373d5764617a709c6ca23118f5f4001cf
alucardthefish <sergiop18@gmail.com>
2020-03-05 00:18:01 -0500

    Add logic for displaying data in a recycler view

--- app/src/main/java/com/sop/cacapp/Fragments/FecalDiaryFragment.java
+++ app/src/main/java/com/sop/cacapp/Fragments/FecalDiaryFragment.java
@@ -2,66 +2,85 @@ package com.sop.cacapp.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.sop.cacapp.Adapters.PoopOccurrenceAdapter;
+import com.sop.cacapp.Object.PoopOccurrence;
+import com.sop.cacapp.Persistence.PoopOccurrencePersistent;
 import com.sop.cacapp.R;
 
+import java.util.ArrayList;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FecalDiaryFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class FecalDiaryFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    private PoopOccurrenceAdapter poopOccurrenceAdapter;
+    private RecyclerView recyclerViewPoopOccurrence;
+    private ArrayList<PoopOccurrence> poopOccurrenceArrayList;
 
     public FecalDiaryFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FecalDiaryFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FecalDiaryFragment newInstance(String param1, String param2) {
-        FecalDiaryFragment fragment = new FecalDiaryFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fecal_diary, container, false);
+        View view = inflater.inflate(R.layout.fragment_fecal_diary, container, false);
+        recyclerViewPoopOccurrence = view.findViewById(R.id.recyclerViewDiary);
+        poopOccurrenceArrayList = new ArrayList<>();
+        // Load list
+        loadList();
+        return view;
+    }
+
+    public void loadList() {
+        CollectionReference poopOccurrencesReference = new PoopOccurrencePersistent().getmReference();
+        poopOccurrencesReference
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                        if (e != null) {
+                            Log.d("snaplistener", "Listen Failed on loadList method of FecalDiaryFragment.");
+                            return;
+                        }
+                        if (!queryDocumentSnapshots.isEmpty()) {
+                            Log.d("snaplistener", "La cantidad de datos son: " + queryDocumentSnapshots.size());
+                            for (DocumentSnapshot ds : queryDocumentSnapshots.getDocuments()) {
+                                PoopOccurrence poopOccurrence = ds.toObject(PoopOccurrence.class);
+                                poopOccurrenceArrayList.add(poopOccurrence);
+                            }
+                            // Show data
+                            showData();
+                        }
+                    }
+                });
+    }
+
+    public void showData() {
+        recyclerViewPoopOccurrence.setLayoutManager(new LinearLayoutManager(getContext()));
+        poopOccurrenceAdapter = new PoopOccurrenceAdapter(getContext(), poopOccurrenceArrayList);
+        recyclerViewPoopOccurrence.setAdapter(poopOccurrenceAdapter);
     }
 }
