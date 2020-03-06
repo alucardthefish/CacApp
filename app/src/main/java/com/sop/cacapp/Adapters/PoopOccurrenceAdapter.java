d4ddd5a544369b0b3463bdc86d4a367a02449ba2
alucardthefish <sergiop18@gmail.com>
2020-03-06 14:18:08 -0500

    Add the deposition satisfaction in a ratingBar and set icon according to that value

--- app/src/main/java/com/sop/cacapp/Adapters/PoopOccurrenceAdapter.java
+++ app/src/main/java/com/sop/cacapp/Adapters/PoopOccurrenceAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -49,15 +50,17 @@ public class PoopOccurrenceAdapter extends RecyclerView.Adapter<PoopOccurrenceAd
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Timestamp poopOccurrenceDateTime = model.get(position).getOccurrenceTime();
-        // TODO add the satisfaction in a ratingBar and set icon according to that value
         float poopOccurrenceSatisfaction = model.get(position).getSatisfaction();
         Date date = poopOccurrenceDateTime.toDate();
         String patternOne = "dd MMMM yyyy";
         String patternTwo = "hh:mm aa";
         Locale current = mContext.getResources().getConfiguration().locale;
+        holder.setSatisfactionIcon(poopOccurrenceSatisfaction);
         holder.tvDate.setText(new SimpleDateFormat(patternOne, current).format(date));
         holder.tvTime.setText(new SimpleDateFormat(patternTwo, current).format(date));
+        holder.rbStars.setRating(poopOccurrenceSatisfaction);
     }
+
 
     @Override
     public int getItemCount() {
@@ -77,12 +80,26 @@ public class PoopOccurrenceAdapter extends RecyclerView.Adapter<PoopOccurrenceAd
         ImageView ivDepositionIcon;
         TextView tvDate;
         TextView tvTime;
+        RatingBar rbStars;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             ivDepositionIcon = itemView.findViewById(R.id.ivDepositionIcon);
             tvDate = itemView.findViewById(R.id.tvDate);
             tvTime = itemView.findViewById(R.id.tvTime);
+            rbStars = itemView.findViewById(R.id.rbStars);
+        }
+
+        public void setSatisfactionIcon(float rating) {
+            if (rating >= 0 && rating <= 1) {
+                ivDepositionIcon.setImageResource(R.drawable.shit1);
+            } else if (rating > 1 && rating <= 2) {
+                ivDepositionIcon.setImageResource(R.drawable.shit2);
+            } else if (rating > 3 && rating <= 4) {
+                ivDepositionIcon.setImageResource(R.drawable.shit4);
+            } else if (rating > 4 && rating <= 5) {
+                ivDepositionIcon.setImageResource(R.drawable.shit5);
+            }
         }
     }
 }
