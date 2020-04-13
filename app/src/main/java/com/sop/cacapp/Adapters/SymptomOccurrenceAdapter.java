6ed8d5000cfa7b0067de4f789346fa1e947acd19
alucardthefish <sergiop18@gmail.com>
2020-04-12 23:02:10 -0500

    Add recycler elements selectable
    Add logic and methods in SymptomOccurrenceAdapter class for allowing recycler items to be selectable
    All the functionality works together with the ActionMode class which will let those selected symptoms to be editable or deletable.

--- app/src/main/java/com/sop/cacapp/Adapters/SymptomOccurrenceAdapter.java
+++ app/src/main/java/com/sop/cacapp/Adapters/SymptomOccurrenceAdapter.java
@@ -1,9 +1,12 @@
 package com.sop.cacapp.Adapters;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,26 +19,26 @@ import com.sop.cacapp.R;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
-public class SymptomOccurrenceAdapter extends RecyclerView.Adapter<SymptomOccurrenceAdapter.ViewHolder> implements View.OnClickListener {
+public class SymptomOccurrenceAdapter extends RecyclerView.Adapter<SymptomOccurrenceAdapter.ViewHolder> {
 
     private LayoutInflater inflater;
     private ArrayList<Symptom> symptomsList;
     private View.OnClickListener listener;
     private Context context;
 
+    private SparseBooleanArray selectedSymptoms;
+    private int currentSelectedIdx = -1;
+
+    private OnClickListener onClickListener = null;
+
     public SymptomOccurrenceAdapter(Context context, ArrayList<Symptom> symptomsList) {
         this.inflater = LayoutInflater.from(context);
         this.symptomsList = symptomsList;
+        this.selectedSymptoms = new SparseBooleanArray();
         this.context = context;
-    }
-
-    @Override
-    public void onClick(View v) {
-        if (listener != null) {
-            listener.onClick(v);
-        }
     }
 
 
@@ -44,28 +47,94 @@ public class SymptomOccurrenceAdapter extends RecyclerView.Adapter<SymptomOccurr
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         // Inflate the element view of the recycler view
         View view = inflater.inflate(R.layout.symptom_recycler_element, parent, false);
-        view.setOnClickListener(this);
         return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
         String patternOne = "dd MMMM yyyy";
         String patternTwo = "hh:mm aa";
         Locale current = context.getResources().getConfiguration().locale;
-        Symptom currentSymptom = symptomsList.get(position);
+        final Symptom currentSymptom = symptomsList.get(position);
         holder.tvSymptomDate.setText(new SimpleDateFormat(patternOne, current).format(currentSymptom.toDate()));
         holder.tvSymptomTime.setText(new SimpleDateFormat(patternTwo, current).format(currentSymptom.toDate()));
         holder.rbSymptomIntensity.setRating(currentSymptom.getIntensity());
         holder.tvSymptomDescription.setText(currentSymptom.getDescription().length() > 50 ?
                 String.format(currentSymptom.getDescription().substring(0, 49) + "%s", "...") :
                 currentSymptom.getDescription());
+
         // When pics gathered call the pic chooser method here
+
+        holder.element.setActivated(selectedSymptoms.get(position, false));
+
+        holder.element.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (onClickListener != null) {
+                    onClickListener.onItemClick(v, position, currentSymptom);
+                }
+            }
+        });
+
+        holder.element.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                if (onClickListener != null) {
+                    onClickListener.onItemLongClick(v, position, currentSymptom);
+                }
+                return true;
+            }
+        });
+
+        toggleChecked(holder, position);
+
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return super.getItemId(position);
     }
 
     @Override
     public int getItemCount() {
         return symptomsList.size();
+    }
+
+    public int getSelectedSymptomsCount() {
+        return selectedSymptoms.size();
+    }
+
+    private void toggleChecked(ViewHolder holder, int position) {
+        if (selectedSymptoms.get(position, false)) {
+            holder.element.setCardBackgroundColor(Color.LTGRAY);
+        } else {
+            holder.element.setCardBackgroundColor(Color.WHITE);
+        }
+    }
+
+    public void toggleSelection(int pos) {
+        currentSelectedIdx = pos;
+        if (selectedSymptoms.get(pos, false)) {
+            selectedSymptoms.delete(pos);
+        } else {
+            selectedSymptoms.put(pos, true);
+        }
+        notifyItemChanged(pos);
+    }
+
+    public void clearSelectedSymptoms() {
+        selectedSymptoms.clear();
+        notifyDataSetChanged();
+    }
+
+    public List<Symptom> getSelectedSymptoms() {
+        List<Symptom> symptoms = new ArrayList<>(selectedSymptoms.size());
+        for (int i = 0; i < selectedSymptoms.size(); i++) {
+            if (selectedSymptoms.valueAt(i)) {
+                symptoms.add(symptomsList.get(i));
+            }
+        }
+        return symptoms;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
@@ -76,9 +145,11 @@ public class SymptomOccurrenceAdapter extends RecyclerView.Adapter<SymptomOccurr
         TextView tvSymptomTime;
         RatingBar rbSymptomIntensity;
         TextView tvSymptomDescription;
+        CardView element;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            element = itemView.findViewById(R.id.symptomCardView);
             ivSymptomPicture = itemView.findViewById(R.id.ivSymptomPicture);
             tvSymptomDate = itemView.findViewById(R.id.tvSymptomDate);
             tvSymptomTime = itemView.findViewById(R.id.tvSymptomTime);
@@ -87,6 +158,15 @@ public class SymptomOccurrenceAdapter extends RecyclerView.Adapter<SymptomOccurr
         }
 
         //Then could be change the symptom picture according to intensity, very similar as in defecation part
+    }
+
+    public interface OnClickListener {
+        void onItemClick(View view, int position, Symptom symptom);
+        void onItemLongClick(View view, int position, Symptom symptom);
+    }
+
+    public void setOnClickListener(OnClickListener onClickListener) {
+        this.onClickListener = onClickListener;
     }
 
 }
--- app/src/main/java/com/sop/cacapp/Fragments/SymptomsMainFragment.java
+++ app/src/main/java/com/sop/cacapp/Fragments/SymptomsMainFragment.java
@@ -3,14 +3,19 @@ package com.sop.cacapp.Fragments;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.ActionMode;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -24,6 +29,7 @@ import com.google.firebase.firestore.QuerySnapshot;
 import com.sop.cacapp.Adapters.SymptomOccurrenceAdapter;
 import com.sop.cacapp.Classes.Symptom;
 import com.sop.cacapp.CustomViewClasses.CustomRecyclerView;
+import com.sop.cacapp.LoaderDialog;
 import com.sop.cacapp.Persistence.SymptomPersistent;
 import com.sop.cacapp.R;
 
@@ -41,6 +47,9 @@ public class SymptomsMainFragment extends Fragment {
     private TextView emptyView;
     private FloatingActionButton fabAddSymptom;
     private ListenerRegistration loadSymptomsListener;
+    private LoaderDialog loaderDialog;
+
+    private ActionMode actionMode;
 
     public SymptomsMainFragment() {
         // Required empty public constructor
@@ -57,16 +66,20 @@ public class SymptomsMainFragment extends Fragment {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_symptoms_main, container, false);
 
+
+
         emptyView = view.findViewById(R.id.tvNoSymptoms);
         fabAddSymptom = view.findViewById(R.id.fabAddSymptom);
+        loaderDialog = new LoaderDialog(view.getContext());
+        loaderDialog.startLoading();
 
         customRecyclerView = view.findViewById(R.id.customRecyclerView);
-        customRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        customRecyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
         customRecyclerView.setEmptyView(emptyView);
 
         symptomArrayList = new ArrayList<>();
 
-        symptomAdapter = new SymptomOccurrenceAdapter(getContext(), symptomArrayList);
+        symptomAdapter = new SymptomOccurrenceAdapter(view.getContext(), symptomArrayList);
 
         customRecyclerView.setAdapter(symptomAdapter);
         //feedRecycler();
@@ -77,6 +90,24 @@ public class SymptomsMainFragment extends Fragment {
                 addSymptom();
                 Snackbar.make(v, "Agregando nuevo sintoma", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        symptomAdapter.setOnClickListener(new SymptomOccurrenceAdapter.OnClickListener() {
+            @Override
+            public void onItemClick(View view, int position, Symptom symptom) {
+                if (symptomAdapter.getSelectedSymptomsCount() > 0) {
+                    enableActionMode(position);
+                } else {
+                    Toast.makeText(view.getContext(), "Se quiere ver el elemento: " + symptom.toString(), Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onItemLongClick(View view, int position, Symptom symptom) {
+                enableActionMode(position);
+
+                Toast.makeText(view.getContext(), "Se quiere seleccionar el elemento " + symptom.toString(), Toast.LENGTH_LONG).show();
             }
         });
 
@@ -100,13 +131,11 @@ public class SymptomsMainFragment extends Fragment {
     }
 
     private void addSymptom() {
-        Symptom s1 = new Symptom("Hola mundo este es mi primer sintoma vamos a ver como nos va con la app", 2.0f);
-        //symptomArrayList.add(s1);
+        //enableActionMode();
+        //Symptom s1 = new Symptom("Hola mundo este es mi primer sintoma vamos a ver como nos va con la app", 2.0f);
 
-        SymptomPersistent symptomPersistent = new SymptomPersistent();
-        symptomPersistent.addSymptom(s1, view);
-
-        //symptomAdapter.notifyDataSetChanged();
+        //SymptomPersistent symptomPersistent = new SymptomPersistent();
+        //symptomPersistent.addSymptom(s1, view);
     }
 
     private void loadSymptoms() {
@@ -121,6 +150,8 @@ public class SymptomsMainFragment extends Fragment {
                             return;
                         }
 
+                        //loaderDialog.startLoading();
+                        Log.d("loaderDialog", "start");
                         String source = queryDocumentSnapshots != null &&
                                 queryDocumentSnapshots.getMetadata().hasPendingWrites() ? "Local" : "Server";
 
@@ -154,6 +185,8 @@ public class SymptomsMainFragment extends Fragment {
                                 }
                                 counter++;
                             }
+                            loaderDialog.stopLoading();
+                            Log.d("loaderDialog", "stop");
                             symptomAdapter.notifyDataSetChanged();
                         }
                     }
@@ -165,5 +198,73 @@ public class SymptomsMainFragment extends Fragment {
         super.onDestroy();
         Log.d("SMF", "on Destroy fragment");
         loadSymptomsListener.remove();
+        loaderDialog.stopLoading();
+    }
+
+    private void enableActionMode(int position) {
+        if (actionMode == null) {
+            AppCompatActivity mainActivity = ((AppCompatActivity) getActivity());
+            actionMode = mainActivity.startSupportActionMode(new ActionModeCallback());
+        }
+        toggleSelection(position);
+    }
+
+    private void toggleSelection(int position) {
+        symptomAdapter.toggleSelection(position);
+        int count = symptomAdapter.getSelectedSymptomsCount();
+        final int EDITION_ITEM = 0;
+
+        if (count == 0) {
+            actionMode.finish();
+            fabAddSymptom.show();
+        } else {
+            if (count == 1) {
+                actionMode.getMenu().getItem(EDITION_ITEM).setVisible(true);
+            } else {
+                actionMode.getMenu().getItem(EDITION_ITEM).setVisible(false);
+            }
+            actionMode.setTitle(String.valueOf(count));
+            actionMode.invalidate();
+            fabAddSymptom.hide();
+        }
+    }
+
+    private class ActionModeCallback implements ActionMode.Callback {
+
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            mode.getMenuInflater().inflate(R.menu.symptoms_menu, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            boolean flag = false;
+            int id = item.getItemId();
+            switch (id) {
+                case R.id.action_delete:
+                    //deleteInboxes();
+                    mode.finish();
+                    fabAddSymptom.show();
+                    flag = true;
+                    break;
+                default:
+                    break;
+            }
+
+            return flag;
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+            symptomAdapter.clearSelectedSymptoms();
+            actionMode = null;
+            fabAddSymptom.show();
+        }
     }
 }
